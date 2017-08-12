217a11a70baf5ff0d515dc6fa3b2656c70ed579e
Dear <369574757@qq.com>
2017-08-12 13:52:35 +0800

    class4

---
+++ class4/work.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import sys
+
+# 函数的意义
+
+def a():
+    pass
+
+# 定义和调用规则
+'''
+定义函数
+写漂亮的文档字串
+返回值
+调用函数
+接收返回值
+内置函数与自定义函数
+'''
+
+def func(x, y:int=3 z=1, *args, **kwargs) -> int:
+    '''
+    function func
+    :param z
+    '''
+
+    return x+y+z
+
+#
