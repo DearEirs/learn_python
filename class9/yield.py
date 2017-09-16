1783bd7a67434c536ff8d4ee6a9cbd3a85de01ce
DearEirs <369574757@qq.com>
2017-09-16 17:17:43 +0800

    Update yield.py

--- class9/yield.py
+++ class9/yield.py
@@ -14,6 +14,9 @@ files = ['file1', 'file2', 'file3', 'file4']
 
 
 def blocking_func(file):
+    '''
+    阻塞
+    '''
     new_file = os.open('new1', os.O_WRONLY | os.O_NONBLOCK)
     new_file = os.fdopen(new_file, 'a')
     with open(file) as f:
@@ -23,6 +26,9 @@ def blocking_func(file):
 
 
 def futures_gen():
+    ···
+    生成器,启用线程池执行文件读写操作,并返回Future对象
+    ···
     for file in files:
         future = pool.submit(blocking_func, file)
         yield future
@@ -33,6 +39,7 @@ def coro_way():
     for future in futures_gen():
         futures.append(future)
     # return [future.result() for future in futures]
+    # 这里的return会阻塞进程 等待文件完成写入
 
 
 if __name__ == '__main__':
@@ -47,3 +54,11 @@ if __name__ == '__main__':
                 data = f.read()
                 nf.writelines(data)
     print(time.time() - start)
+
+ 
+'''
+1. 调用coro_way函数
+2. 访问生成器
+3. 生成器开启线程执行文件读写操作
+4. 程序返回coro_way函数 继续执行代码
+'''
